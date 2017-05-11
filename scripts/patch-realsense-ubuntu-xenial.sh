f3aebebf97cabbe41577fa9ce6bd11c24f765275
Martina Kollarova <martina.kollarova@intel.com>
2017-05-11 11:52:41 +0300

    Clone correct kernel version for patching
    The top of the release branch (e.g. ubuntu-xenial) might not match the
    current kernel version if it hasn't been updated, resulting with an
    error in modprobe "uvcvideo: version magic X should be Y". This patch
    fixes that, with the added bonus that it works with any Ubuntu release.

--- scripts/patch-realsense-ubuntu-xenial.sh
+++ scripts/patch-realsense-ubuntu-xenial.sh
@@ -17,12 +17,12 @@ source ./scripts/patch-utils.sh
 require_package libusb-1.0-0-dev
 require_package libssl-dev
 
-LINUX_BRANCH=$(uname -r)
-kernel_name="ubuntu-xenial"
-
-# Download the latest the linux kernel sources from ubuntu-xenial tree
-[ ! -d ${kernel_name} ] && git clone git://kernel.ubuntu.com/ubuntu/ubuntu-xenial.git --depth 1
-cd ${kernel_name}
+if [ ! -d linux-image-$(uname -r) ]
+    git clone \
+        git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git \
+        --depth 1 --branch $(uname -r)
+fi
+cd linux-image-$(uname -r)
 
 # Verify that there are no trailing changes., warn the user to make corrective action if needed
 if [ $(git status | grep 'modified:' | wc -l) -ne 0 ];
