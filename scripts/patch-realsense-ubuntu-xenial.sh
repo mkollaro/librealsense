7607a2cb7ddd1244a145a3904695cdbf57aa1658
Martina Kollarova <martina.kollarova@intel.com>
2017-05-11 15:46:50 +0300

    kernel dir

--- scripts/patch-realsense-ubuntu-xenial.sh
+++ scripts/patch-realsense-ubuntu-xenial.sh
@@ -17,17 +17,17 @@ source ./scripts/patch-utils.sh
 require_package libusb-1.0-0-dev
 require_package libssl-dev
 
-if [ ! -d kernel ]; then
+KERNEL_DIR="linux_$(uname -r | cut -d '-' -f 1)"
+if [ ! -d $KERNEL_DIR ]; then
     source /etc/lsb-release
     RELEASE=$DISTRIB_CODENAME
 	echo -e "\e[36mEnabling sources in /etc/apt/sources.list\e[0m"
     sudo sh -c "echo \"deb-src http://us.archive.ubuntu.com/ubuntu/ $RELEASE main restricted\" >> /etc/apt/sources.list"
     sudo apt-get update
 	echo -e "\e[36mDownloading Linux source code\e[0m"
-    apt-get source linux-image-$(uname -r) --download-only
-    tar xf linux-image-$(uname -r).tar.gz kernel
+    apt-get source linux-image-$(uname -r)
 fi
-cd kernel
+cd $KERNEL_DIR
 
 #Check if we need to apply patches or get reload stock drivers (Developers' option)
 [ "$#" -ne 0 -a "$1" == "reset" ] && reset_driver=1 || reset_driver=0
